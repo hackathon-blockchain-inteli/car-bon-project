786c198257b8378feb7f8b5d21cf4626c8e2c153
ViniciosLugli <vinicioslugli@gmail.com>
2023-05-07 08:59:21 -0300

    feat(frontend): :sparkles: Make function generate card of exchange

--- frontend/src/pages/Exchange/Exchange.tsx
+++ frontend/src/pages/Exchange/Exchange.tsx
@@ -1,42 +1,51 @@
 import React from 'react';
-import styles from "./Exchange.module.scss";
+import styles from './Exchange.module.scss';
 import NavBar from '../../componentes/NavBar/NavBar';
 import FootBar from '../../componentes/FootBar/FootBar';
 import tst from '../../assets/tst.png';
 
-const Exchange: React.FC = () => {
-  return (
-    <div>
-        <NavBar />
-        <div className={styles.container}>
-            <div className={styles.intro}>
-                <h1>Exchange</h1>
-                <div>Token's value: 100 BTG dol</div>
-            </div>
-            <div className={styles.cardList}>
-                <div>
-                    <img src={tst}></img>
-                    <div>
-                        <p><span>Car id: </span>786867</p>
-                        <p><span>Tokens: </span>24</p>
-                        <p><span>Wallet adress: </span>gkfjdhsk67r5bjijnjk6fg</p>
-                    </div>
-                    <button>Buy Tokens</button>
-                </div>
-                <div>
-                    <img src={tst}></img>
-                    <div>
-                        <p><span>Car id: </span>786867</p>
-                        <p><span>Tokens: </span>24</p>
-                        <p><span>Wallet adress: </span>gkfjdhsk67r5bjijnjk6fg</p>
-                    </div>
-                    <button>Buy Tokens</button>
-                </div>
-            </div>
-        </div>
-        <FootBar />
-    </div>
-  );
+function generateCard(id: number, tokens: number, wallet: string) {
+	return (
+		<div>
+			<img src={tst}></img>
+			<div>
+				<p>
+					<span>Car id: </span>
+					{id}
+				</p>
+				<p>
+					<span>Tokens: </span>
+					{tokens}
+				</p>
+				<p>
+					<span>Wallet adress: </span>
+					{wallet}
+				</p>
+			</div>
+			<button>Buy Tokens</button>
+		</div>
+	);
 }
+
+const Exchange: React.FC = () => {
+	return (
+		<div>
+			<NavBar />
+			<div className={styles.container}>
+				<div className={styles.intro}>
+					<h1>Exchange</h1>
+					<div>Token's value: 100 BTG dol</div>
+				</div>
+				<div className={styles.cardList}>
+					{generateCard(1, 100, '0x123456789')}
+					{generateCard(2, 100, '0x123456789')}
+					{generateCard(3, 100, '0x123456789')}
+				</div>
+				<div className={styles.cardList}></div>
+			</div>
+			<FootBar />
+		</div>
+	);
+};
 
 export default Exchange;
